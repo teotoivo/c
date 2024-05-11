d43c461494ea50bd61505c1f65b227979a10cbd1
teotoivo <teo.maximilien@gmail.com>
2024-05-11 13:45:57 +0300

    continued todo

--- 3. Todo app/main.c
+++ 3. Todo app/main.c
@@ -31,7 +31,7 @@ void checkIfMallocFailed(void *ptr)
 	return;
 }
 
-void addTodo(TodoStruct *pTodoStruct, Todo *pNewTodo)
+void addTodo(TodoStruct *pTodoStruct, char *newName, char *newDescription)
 {
 	bool isBufferInvalid = pTodoStruct->length > pTodoStruct->bufferSize;
 	if (isBufferInvalid)
@@ -43,12 +43,23 @@ void addTodo(TodoStruct *pTodoStruct, Todo *pNewTodo)
 	bool todosBufferIsFull =  pTodoStruct->length == pTodoStruct->bufferSize;;
 	if (todosBufferIsFull)
 	{
-	  Todo **tempArray = pTodoStruct->arr;
-
-		pTodoStruct->bufferSize *= 2;
-		pTodoStruct->arr = realloc(pTodoStruct->arr, (sizeof(Todo*) * pTodoStruct->bufferSize));
+	
+		Todo **tempArray = pTodoStruct->arr;
+		pTodoStruct->bufferSize *= BUFFER_MULTIPLIER;
+		Todo **newArray = realloc(pTodoStruct->arr, (sizeof(Todo*) * pTodoStruct->bufferSize));
+		if (newArray == NULL) {
+			fprintf(stderr, "Memory reallocation failed\n");
+			exit(EXIT_FAILURE);
+		}
+		pTodoStruct->arr = newArray;
 
 	}
+
+	Todo *pNewTodo = malloc(sizeof(Todo));
+	checkIfMallocFailed(pNewTodo);
+	strcpy(pNewTodo->name, newName);
+	strcpy(pNewTodo->description, newDescription);
+
 	pTodoStruct->arr[pTodoStruct->length] = pNewTodo;
 	
 	pTodoStruct->length++;
@@ -58,7 +69,13 @@ void addTodo(TodoStruct *pTodoStruct, Todo *pNewTodo)
 void getAllTodos(TodoStruct *pTodoStruct)
 {
 	for (int i = 0; i < pTodoStruct->length; i++) {
-		printf("\nName: %s\nDescription: %s\n", pTodoStruct->arr[i]->name, pTodoStruct->arr[i]->description);
+		printf("\nTodos id: %d\nName: %s\nDescription: %s\n", i+1, pTodoStruct->arr[i]->name, pTodoStruct->arr[i]->description);
+	}
+
+	printf("Press enter to continue...");
+
+	if (getchar() != '\n') {
+		while(getchar() != '\n');
 	}
 }
 
@@ -72,7 +89,7 @@ void initTodos(TodoStruct *todoStruct, size_t initialSize)
 
 int main()
 {
-	printf("Todo app.\n\n");
+	printf("Todo app.");
 
 	
 	TodoStruct todoStruct;
@@ -83,15 +100,14 @@ int main()
 	while (userAnswer != 'q')
 	{
 		
-		printf("What do you want to do?\n");
-
-		printf("t test\n");
+		printf("\n\nWhat do you want to do?\n");
 
 		printf("a  Get all todos?\n");
 		printf("b  Get a todo by its id?\n");
 		printf("c  Get a todo by its name?\n");
 		printf("d  Add a todo?\n");
-		printf("e  Delete a todo\n\n");
+		printf("e  Edit a todo?\n");
+		printf("f  Delete a todo\n\n");
 
 		printf("q Quit.\n\n");
 
@@ -100,25 +116,88 @@ int main()
 		scanf("%1s", tempChar);
 		userAnswer = tempChar[0];
 
+		// Clear any extra characters from the terminal if there are any
+		while (getchar() != '\n');
+
+
+
+
 		printf("\n\n");
 
 		switch (userAnswer)
 		{
-			case 't':
-
-				Todo tempTodo = {"test", "test ttt"};
-
-				Todo *pPassTodo = malloc(sizeof(Todo));
-				*pPassTodo = tempTodo;
-				
-				addTodo(&todoStruct, pPassTodo);
-
-				break;
-
 			case 'a':
 
 				getAllTodos(&todoStruct);
 
+				break;
+
+
+			case 'b':
+
+				printf("\nEnter todos id: ");
+				
+				char ans = fgetc(stdin);
+
+				if (!strchr(ans, '\n')) {
+					while(fgetc(stdin)!='\n');
+				}
+
+				break;
+
+
+			case 'c':
+
+				break;
+
+
+
+			case 'd':
+
+				char name[MAX_NAME_LENGTH];
+				char description[MAX_DESCRIPTION_LENGTH];
+
+				printf("\nEnter the name for the todo: ");
+				fgets(name, sizeof(name), stdin);
+
+				//clears extra characters that were left over and if not removes the \n
+				if (!strchr(name, '\n')) {
+					while(fgetc(stdin)!='\n');
+				} else {
+					name[strcspn(name, "\n")] = '\0';
+				}
+
+				printf("Enter the description for the todo: ");
+				fgets(description, sizeof(description), stdin);
+				
+				//clears extra characters that were left over and if not removes the \n
+				if (!strchr(description, '\n')) {
+					while(fgetc(stdin)!='\n');
+				} else {
+					name[strcspn(description, "\n")] = '\0';
+				}
+
+				addTodo(&todoStruct, name, description);
+
+				printf("\nTodo created!");
+
+				break;
+
+
+
+			case 'e':
+
+				break;
+
+
+			case 'f':
+
+				break;
+
+
+
+			case 'q':
+				printf("Bye bye!");
 				break;
 
 			default:
